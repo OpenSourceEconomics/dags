3e71658a127d86cb6d666eb7276a6f1ac03a9925
Janos Gabler <janos.gabler@gmail.com>
2022-03-16 13:25:57 +0100

    Add output processing decorators and tests.

---
+++ tests/test_process_output.py
@@ -0,0 +1,71 @@
+from dags.process_output import aggregated_output
+from dags.process_output import dict_output
+from dags.process_output import list_output
+from dags.process_output import single_output
+
+
+def test_single_output_decorator():
+    @single_output
+    def f():
+        return (1,)
+
+    assert f() == 1
+
+
+def test_dict_output_decorator():
+    @dict_output(keys=["a", "b"])
+    def f():
+        return (1, 2)
+
+    assert f() == {"a": 1, "b": 2}
+
+
+def test_list_output_decorator():
+    @list_output
+    def f():
+        return (1, 2)
+
+    assert f() == [1, 2]
+
+
+def test_aggregated_output_decorator():
+    @aggregated_output(aggregator=lambda x, y: x + y)
+    def f():
+        return (1, 2)
+
+    assert f() == 3
+
+
+def test_single_output_direct_call():
+    def f():
+        return (1,)
+
+    g = single_output(f)
+
+    assert g() == 1
+
+
+def test_dict_output_direct_call():
+    def f():
+        return (1, 2)
+
+    g = dict_output(f, keys=["a", "b"])
+
+    assert g() == {"a": 1, "b": 2}
+
+
+def test_list_output_direct_call():
+    def f():
+        return (1, 2)
+
+    g = list_output(f)
+
+    assert g() == [1, 2]
+
+
+def test_aggregated_output_direct_call():
+    def f():
+        return (1, 2)
+
+    g = aggregated_output(f, aggregator=lambda x, y: x + y)
+    assert g() == 3
