9259499ca6694b8db8c4eb8e21d2caf96728bd57
Marvin Immesberger <immesberger@posteo.de>
2025-03-18 10:34:36 +0100

    Add failing test to reproduce error.

--- tests/test_dag_tree.py
+++ tests/test_dag_tree.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import functools
+import inspect
 from typing import TYPE_CHECKING, Literal
 
 import pytest
@@ -630,3 +631,61 @@ def test_partialled_function_argument() -> None:
         functions=tree, targets=targets, input_structure=input_structure
     )
     concatenated_func({"a": 1})
+
+
+@pytest.mark.parametrize(
+    (
+        "functions_tree",
+        "input_structure",
+        "flat_function_name_to_check",
+        "expected_argument_name",
+    ),
+    [
+        (
+            # For some reason, this works:
+            {
+                "common_name": {"foo": lambda x: x},
+                "target_namespace": {
+                    "common_name": {"foo": lambda x: x},
+                    "target_leaf": lambda common_name__foo: common_name__foo,
+                },
+            },
+            {
+                "common_name": {"x": None},
+                "target_namespace": {"common_name": {"x": None}},
+            },
+            "target_namespace__target_leaf",
+            "common_name__foo",
+        ),
+        (
+            # This does not work, returned arg name:
+            # "einkommensteuer__kindergeld__grundsätzlich_anspruchsberechtigt"
+            {
+                "kindergeld": {"grundsätzlich_anspruchsberechtigt": lambda x: x},
+                "einkommensteuer": {
+                    "kindergeld": {"grundsätzlich_anspruchsberechtigt": lambda x: x},
+                    "anzahl_kinderfreibeträge": lambda kindergeld__grundsätzlich_anspruchsberechtigt: kindergeld__grundsätzlich_anspruchsberechtigt,  # noqa: E501, PLC2401
+                },
+            },
+            {
+                "kindergeld": {"x": None},
+                "einkommensteuer": {"kindergeld": {"x": None}},
+            },
+            "einkommensteuer__anzahl_kinderfreibeträge",
+            "kindergeld__grundsätzlich_anspruchsberechtigt",
+        ),
+    ],
+)
+def test_correct_argument_names(
+    functions_tree: NestedFunctionDict,
+    input_structure: NestedInputStructureDict,
+    flat_function_name_to_check: str,
+    expected_argument_name: str,
+) -> None:
+    flat_functions = _flatten_functions_and_rename_parameters(
+        functions_tree, input_structure
+    )
+    assert (
+        expected_argument_name
+        in inspect.signature(flat_functions[flat_function_name_to_check]).parameters
+    )
