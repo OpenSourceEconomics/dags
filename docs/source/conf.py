84b3edbf0d5cbea3871e0a9109f4d54aa5403ec2
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2026-01-12 12:45:47 +0100

    Build docs using pixi.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
-    "sphinx_panels",
+    "sphinx_design",
 ]
 
 autodoc_member_order = "bysource"
@@ -75,7 +75,7 @@ version = ".".join(release.split(".")[:2])  # ty: ignore[invalid-assignment]
 
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
